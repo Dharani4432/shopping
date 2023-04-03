a84595fd7ba343b3a638fac158ad5051c87032ba
Dharani4432 <119032451+Dharani4432@users.noreply.github.com>
2023-04-03 18:19:08 +0530

    Add files via upload

---
+++ src/Cart.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+
+let Cart = (props) => {
+    return (
+        <div className="cart">
+            
+
+            <img src={props.productImage}  alt="  found error  " width="300" height="300" className="product_img" />
+            
+
+            <h1 className="product_font"> {props.productName}  </h1>
+            <h2>Category :  {props.productCategory}  </h2>
+
+            <h3>M.R.P : <i class="fa fa-inr" aria-hidden="true"></i>  <del>{props.oldPrice} </del>  </h3>
+            <h3>Price :  <i class="fa fa-inr" aria-hidden="true"></i> <ins> {props.productPrice}</ins>   <p><i>(Inclusive all taxes)</i></p> </h3>
+            <h4>Rating : {props.productRating}</h4>
+
+            {/* <h4>product Info : {props.productInfo} </h4> */}
+<div className="size">
+            <ul>
+          <li> <h3> SIZE: </h3></li>
+          <li class="bg">7</li>
+          <li class="bg">8</li>
+          <li class="bg">9</li>
+          <li class="bg">10</li>
+          <li class="bg">11</li>
+        </ul>
+        <ul>
+          <li><h3>COLOR:</h3></li>
+          <li class="yellow"></li>
+          <li class="black"></li>
+          <li class="blue"></li>
+        </ul>
+        </div>
+        <center>
+            <button className="button"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Add to cart </button>
+            </center>
+        <br></br>
+
+
+        </div>
+
+    );
+}
+
+export default Cart;
---
+++ src/productData.js
@@ -0,0 +1,155 @@
+const products = [
+    {
+        productName : "Nike ",
+        productPrice : 599.00,
+        oldPrice : 700.00,
+        productInfo : "This is very big product",
+        productCategory : "Silver shine Traditional heels ",
+        productRating : 5,
+        productImage : "https://tse3.mm.bing.net/th?id=OIP.TjRK41GX5eVV45ku3TQFkgHaHa&pid=Api&P=0"
+    },
+    {
+
+        productName : "Puma  ",
+        productPrice : 499.00,
+        oldPrice : 699.00,
+        productInfo : "Best product to ude ",
+        productCategory : "Casual Flats",
+        productRating : 4,
+        productImage : "https://tse2.mm.bing.net/th?id=OIP.n4fZk0LS7vbjJFvScnS9QwHaHa&pid=Api&P=0"
+    },
+    {
+    
+        productName : " H&M ",
+        productPrice : 999.00,
+        oldPrice : 799.00,
+        productInfo : "Now way days every one using  ",
+        productCategory : "Style to ware",
+        productRating : 4,
+        productImage : "https://tse4.mm.bing.net/th?id=OIP.sZGjZMC1QG7QZIQLFvS19AAAAA&pid=Api&P=0"
+    },
+    {
+    
+        productName : " SSS ",
+        productPrice : 599.00,
+        oldPrice : 999.00,
+        productInfo : "Now way days every one using  ",
+        productCategory : "Let’s go to café flats",
+        productRating : 4,
+        productImage : "https://images.streetstylestore.com/1/8/4/0/3/5/184035-vertical_default.jpg" 
+       },
+       {
+       
+        productName : "Campus",
+        productPrice : 579.00,
+        oldPrice : 699.00,
+        productInfo : "Offering Brown Formal Mens Shoes ",
+        productCategory : "party wear show",
+        productRating : 4,
+        productImage : "https://i5.walmartimages.com/asr/4970b54d-9ff0-49ab-9c9b-eeb46a1b2227_1.c76aaec5829e4f6ecc771ea9883e1a22.jpeg" 
+       },
+
+       {
+        
+        productName : "Red Tape",
+        productPrice : 799.00,
+        oldPrice : 999.00,
+        productCategory : "Running shoe",
+        productRating : 5,
+        productImage : "https://tse1.mm.bing.net/th?id=OIP.0dMAsaQ1TtlVOsIFnCYRTQHaHa&pid=Api&P=0" 
+       },
+
+       {
+    
+        productName : "Clarks ",
+        productPrice : 1499.00,
+        oldPrice : 999.00,
+        
+        productCategory : "casual wear",
+        productRating : 4,
+        productImage : "https://tse2.mm.bing.net/th?id=OIP.TqjKTulnzylc3p8bh4ijigHaHa&pid=Api&P=0" 
+       },
+
+       {
+    
+        productName : "Nike",
+        productPrice : 799.00,
+        oldPrice : 999.00,
+
+        productCategory : "stylish flip crocks",
+        productRating : 5,
+        productImage : "https://tse1.mm.bing.net/th?id=OIP.39uatLfNqUSEuQCfu6lDoQHaHa&pid=Api&P=0"
+       },
+
+       {
+       
+        productName : "Reebok",
+        productPrice : 499.00,
+        oldPrice : 999.00,
+        
+        productCategory : "Leather shoe ",
+        productRating : 3,
+        productImage : "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-shoes-1610418585.jpg?crop=0.502xw:1.00xh;0.282xw,0&resize=640:*" 
+       },
+
+       {
+      
+        productName : "Shoetopia",
+        productPrice : 999.00,
+        oldPrice : 1599.00,
+        productInfo : "Formal stylish shoe  ",
+        productCategory : "Style to ware",
+        productRating : 5,
+        productImage : "https://1.bp.blogspot.com/-c7ChZwpb2I8/UitK1tcG4YI/AAAAAAAAAIw/uP-ZNKpFXOY/s1600/Comfortable+Wedges+Sandals+For+Women.jpg" 
+       },
+
+       {
+        
+        productName : "DressBerry",
+        productPrice : 499.00,
+        oldPrice : 999.00,
+
+        productCategory : "Women pumps",
+        productRating : 4,
+        productImage : "https://tse4.mm.bing.net/th?id=OIP.cVMr51A0XeGhiP9SvyFrpQHaEg&pid=Api&P=0" 
+       },
+
+       {
+        
+        productName : "Denil",
+        productPrice : 999.00,
+        oldPrice : 1599.00,
+        productCategory : "Party Ware flats",
+        productRating : 4,
+        productImage : "https://images.streetstylestore.com/2/0/0/3/2/8/200328-vertical_default.jpg" 
+       },
+
+       {
+        
+        productName : "Bata",
+        productPrice : 399.00,
+        oldPrice : 799.00,
+
+        productCategory : "casual wear flats",
+       productRating : 2,
+        productImage : "https://s3-eu-west-1.amazonaws.com/images.linnlive.com/3f25aa9d1567d021dac96f3dcb03559f/e7007f4c-069e-44a2-a755-bda0da7a19b5.jpg" 
+       },
+
+       {
+        
+        productName : "SSS",
+        productPrice : 999.00,
+        oldPrice : 1999.00,
+
+        productCategory : "Ruffle on chain white flat",
+        productRating : 5,
+        productImage : "https://images.streetstylestore.com/2/2/3/4/2/7/223427-vertical_default.jpg" 
+       },
+
+
+
+
+
+];
+
+export default products;
